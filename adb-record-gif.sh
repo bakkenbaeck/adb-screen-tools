63c0847e2ab917dcbe5453e515880226b182ba5e
Tristan King <tristan@bakkenbaeck.no>
2018-01-18 12:19:30 +0100

    add multi device support and tool sanity checking

--- adb-record-gif.sh
+++ adb-record-gif.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+if ! command -v adb > /dev/null; then
+    echo "Missing adb";
+    exit 1
+fi
+if ! command -v ffmpeg  > /dev/null; then
+    echo "Missing ffmpeg";
+    exit 1
+fi
+
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 filename="/sdcard/$timestamp.mp4"
+if [ -z $1 ]; then
+    device=$(adb devices | grep '^.\+\sdevice$' | head -1 | cut -f 1)
+    if [ -z $device ]; then
+        echo "Could not find a device"
+        exit 1
+    fi
+else
+    device=$1
+fi
 
 function finish {
   sleep 1
   echo "\nPulling file..."
-  adb pull $filename
-  adb shell rm $filename
+  adb -s $device pull $filename
+  adb -s $device shell rm $filename
 
   echo "Making gif!"
   ffmpeg -y -i "$timestamp.mp4" -vf "fps=30,scale=640:-1:flags=lanczos,palettegen" palette.png
@@ -22,6 +40,4 @@ function finish {
 trap finish EXIT
 
 echo "Recording $filename... Press Ctrl + C to end the recording"
-adb shell screenrecord $filename
-
-
+adb -s $device shell screenrecord $filename
